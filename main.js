9dd41a60f197f0610aeb5e9d9f1367267aef5909
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-10-19 12:53:07 +0500

    edite-1

--- main.js
+++
@@ -1,6 +0,0 @@
-var main = document.querySelector("#main");
-var desk = document.querySelector(".desk");
-main.addEventListener('mousemove' , function(acc){
-    desk.style.left = acc.x + "px"
-    desk.style.top = acc.y + "px"
-})
